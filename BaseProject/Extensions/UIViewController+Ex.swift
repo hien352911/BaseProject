45d0eb95c75684303b39b370f323744442450323
hien.bui <yuusaku.kd352911@gmail.com>
2018-09-13 10:21:28 +0700

    add toolbar

--- BaseProject/Extensions/UIViewController+Ex.swift
+++ BaseProject/Extensions/UIViewController+Ex.swift
@@ -82,3 +82,32 @@ extension UIViewController {
         UIApplication.shared.isStatusBarHidden = false
     }
 }
+
+// MARK: - UITextFieldDelegate
+extension UIViewController: UITextFieldDelegate {
+    func addToolBar(textField: UITextField) {
+        let toolBar = UIToolbar()
+        toolBar.barStyle = .default
+        toolBar.isTranslucent = true
+        toolBar.tintColor = UIColor.blue
+        let doneButton = UIBarButtonItem(title: "Done", style: .done, target: self, action: #selector(donePressed))
+        let cancelButton = UIBarButtonItem(title: "Cancel", style: .plain, target: self, action: #selector(cancelPressed))
+        let spaceButton = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        toolBar.setItems([cancelButton, spaceButton, doneButton], animated: false)
+        
+        
+        toolBar.isUserInteractionEnabled = true
+        toolBar.sizeToFit()
+        
+        textField.delegate = self
+        textField.inputAccessoryView = toolBar
+    }
+    
+    @objc func donePressed() {
+        view.endEditing(true)
+    }
+    
+    @objc func cancelPressed() {
+        view.endEditing(true) // or do something
+    }
+}
