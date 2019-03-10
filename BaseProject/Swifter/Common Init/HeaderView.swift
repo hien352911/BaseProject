c02b6b6efc6b4c60cc7402a576bbb0baeecca257
hien.bui <yuusaku.kd352911@gmail.com>
2019-03-10 09:31:11 +0700

    add common init file xib

---
+++ BaseProject/Swifter/Common Init/HeaderView.swift
@@ -0,0 +1,27 @@
+//
+//  HeaderView.swift
+//  BaseProject
+//
+//  Created by MTQ on 3/10/19.
+//  Copyright © 2019 MTQ. All rights reserved.
+//
+
+import UIKit
+
+class HeaderView: UIView {
+	
+	@IBOutlet private var contentView: UIView!
+
+	required init?(coder aDecoder: NSCoder) {
+		super.init(coder: aDecoder)
+		commonInit()
+	}
+	
+	private func commonInit() {
+		Bundle.main.loadNibNamed("HeaderView", owner: self)
+		addSubview(contentView)
+		contentView.frame = self.bounds
+		contentView.autoresizingMask = [.flexibleHeight, .flexibleWidth]
+	}
+
+}
